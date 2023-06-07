2aeeb0ba919331e089243f1e0d8bdeb0803b48ec
Natanael Bezerra <nsbbezerra@hotmail.com>
2023-06-07 13:27:55 -0300

    change interceptors axios

--- src/configs/api.ts
+++ src/configs/api.ts
@@ -2,13 +2,22 @@ import axios from "axios";
 
 const apiUrl = import.meta.env.VITE_API_ENDPOINT;
 
-const token = localStorage.getItem("token");
-
 const api = axios.create({
   baseURL: apiUrl || "",
-  headers: {
-    Authorization: token,
-  },
 });
+
+api.interceptors.request.use(
+  (config: any) => {
+    const token = localStorage.getItem("token");
+
+    if (token && config.headers)
+      config.headers.set("Authorization", `${token}`);
+
+    return config;
+  },
+  (error) => {
+    Promise.reject(error);
+  }
+);
 
 export { api, apiUrl };
